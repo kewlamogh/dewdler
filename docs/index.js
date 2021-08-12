7c841263d2758be9c4016bc5c85dd542ee64a4aa
kewlamogh <amogh_sarma@outlook.com>
2021-08-12 08:26:13 -0700

    Added line color & eraser

--- docs/index.js
+++ docs/index.js
@@ -1,13 +1,13 @@
 let pen = document.getElementById("c").getContext("2d");
 let mouse = {isDown: false};
-let line = {size: 10};
-document.body.onload = (e) => {
+let line = {size: 10, color: "black"};
+document.body.onload = e => {
     mouse = {
         isDown: false,
         pos: {x: e.clientX, y: e.clientY}
     }
 }
-document.body.onmousemove = (e) => {
+document.body.onmousemove = e => {
     mouse.pos.x = e.clientX;
     mouse.pos.y = e.clientY;
 }
@@ -17,13 +17,17 @@ document.body.onmousedown = () => {
 document.body.onmouseup = () => {
     mouse.isDown = false;
 }
+document.body.onkeydown = ev => {
+    line.color = (ev.keyCode == 16 ? prompt("Enter new color (or type 'eraser' for the eraser)") : line.color) 
+}
 
 function main() {
     if (mouse.isDown) {
         pen.lineWidth = line.size;
+        pen.strokeStyle = (line.color == "eraser" ? "lightgray" : line.color);
         pen.beginPath();
-        pen.moveTo(mouse.pos.x, mouse.pos.y);
-        pen.lineTo(mouse.pos.x + 1, mouse.pos.y);
+        pen.moveTo(mouse.pos.x, mouse.pos.y + 50);
+        pen.lineTo(mouse.pos.x + 1, mouse.pos.y + 50);
         pen.stroke();
     }
     requestAnimationFrame(main);
